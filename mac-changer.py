584350437a6fe947b02d75ff5824333ca99686e3
IT Master Services <malbin@itmsnv.com>
2019-12-08 18:37:04 -0500

    Update mac-changer.py

--- mac-changer.py
+++ mac-changer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# This program was created by Mark D. Albin. Use at your own risk.
+
 import subprocess
 import optparse
 import re
@@ -24,16 +26,28 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read the MAC address.")
+
+
 options = get_arguments()
-# change_mac(options.interface, options.new_mac)
 
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
+change_mac(options.interface, options.new_mac)
 
-mac_address_search_results = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-
-if mac_address_search_results:
-    print(mac_address_search_results.group(0))
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was changed to " + str(current_mac))
 else:
-    print("[+] Could not read the MAC address.")
+    print("[-] MAC address was not changed")
+
+
+
